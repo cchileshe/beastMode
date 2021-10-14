0945eeccbf79fb000927c24a9579468873089cea
kingrio13 <kingrio.miranda@gmail.com>
2021-10-14 08:13:17 +0800

    Bootstrap added, Node Instapp, App.JS is Updated

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 const path = require('path');
 const express = require('express');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
+const flash = require('connect-flash');
+const cors = require('cors') 
+
 
 const app = express();
+
+
+/**Addding bootstrap to File */
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
+
+
+
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
